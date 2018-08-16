28a4367b1545c2d0e64d9d1ca500bd6e827723d4
moumen <moumen.gamil@vodafone.com>
2018-08-16 21:16:59 +0200

    init test

---
+++ SeleniumWithJenkinsAndMaven/src/test/java/test/TC_001.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_001 {
+
+	@Test
+	public void test() {
+		Assert.assertTrue(true);
+
+	}
+
+	@Test
+	public void test2() {
+		Assert.assertTrue(true);
+
+	}
+
+}
